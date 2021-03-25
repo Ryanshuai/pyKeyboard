c1f8075a3aba2aea56cc752ee27cee7d023a9eb7
Ryan <1018718155@qq.com>
2021-03-25 16:19:51 +0800

    update my keyboard

--- USB Version micropython/main.py
+++ USB Version micropython/main.py
@@ -7,13 +7,13 @@ from send_keys import press_keys, release_keys
 report_count = 10000 * 100
 anyKeyPushed = 0
 
-# to_mat_keys = left_to_mat_key_names
-# lin_io = LinearIO(["B13", "B14", "B15", "A8", "A9", "A10"])
-# mat_io = MatrixIO(["A0", "A1", "A2", "A3", "A4"], ["B8", "B7", "B6", "B5", "B4", "B3"])
-
-to_mat_keys = right_to_mat_key_names
+to_mat_keys = left_to_mat_key_names
 lin_io = LinearIO(["B13", "B14", "B15", "A8", "A9", "A10"])
-mat_io = MatrixIO(["A0", "A1", "A2", "A3", "A4"], ["B0", "B1", "A7", "B3", "B4", "B5"])
+mat_io = MatrixIO(["A0", "A1", "A2", "A3", "A4"], ["B8", "B7", "B6", "B5", "B4", "B3"])
+
+# to_mat_keys = right_to_mat_key_names
+# lin_io = LinearIO(["B13", "B14", "B15", "A8", "A9", "A10"])
+# mat_io = MatrixIO(["A0", "A1", "A2", "A3", "A4"], ["B0", "B1", "A7", "B3", "B4", "B5"])
 
 pyb.delay(1000)  # wait for configuration
 
@@ -27,7 +27,7 @@ while True:
         if len(lin_pushed) + len(mat_pushed):
             # print(lin_pushed, end=" ")
             state_keys = [lin_key[pos] for pos in lin_pushed]
-            fn_num = ("fn1" in state_keys)  # + 2 * ("fn2" in state_keys)
+            fn_num = ("Fn1" in state_keys)  # + 2 * ("fn2" in state_keys)
             mat_keys = [to_mat_keys[fn_num][pos[0]][pos[1]] for pos in mat_pushed]
             print(state_keys, mat_keys)
             press_keys(state_keys, mat_keys)
--- USB Version micropython/send_keys.py
+++ USB Version micropython/send_keys.py
@@ -1,21 +1,37 @@
+# -*- coding: utf-8 -*-
+
 import pyb
 
 hid = pyb.USB_HID()
 
 
 def decode_keys(state_keys, mat_keys):
+    all_keys = state_keys + mat_keys
+
+    all_keys = set(all_keys)
+
     byte0 = 0
-    if "ctrl" in state_keys:
+    if "Ctrl" in all_keys:
         byte0 |= 1
-    if "shift" in state_keys:
+    if "Shift" in all_keys:
         byte0 |= 2
-    if "alt" in state_keys:
+    if "Alt" in all_keys:
         byte0 |= 4
-    if "win" in state_keys:
+    if "Win" in all_keys:
         byte0 |= 8
 
     key_hex_codes = set()
-    for key in mat_keys:
+    for key in all_keys:
+        if isinstance(key, list):
+            key_hex_codes = list()
+            for k in key:
+                hex = key_to_hex.get(k, 0)
+                if isinstance(hex, list):
+                    hex = hex[0]
+                    byte0 |= 2
+                key_hex_codes.append(hex)
+            return byte0, key_hex_codes
+
         hex = key_to_hex.get(key, 0)
         if isinstance(hex, list):
             hex = hex[0]
@@ -72,71 +88,82 @@ key_to_hex = {
     "x": 0x1B,
     "y": 0x1C,
     "z": 0x1D,
-    "_": 0x2D,
+    "_": [0x2D],
 
-    "back": 0x2A,
-    "space": 0x2C,
-    "enter": 0x28,
-    "tab": 0x2B,
-    "esc": 0x29,
-    "delete": 0x4C,
+    "Backspace": 0x2A,
+    "Space": 0x2C,
+    "Enter": 0x28,
+    "Tab": 0x2B,
+    "Esc": 0x29,
+    "Del": 0x4C,
+    "Ins": 0x49,
+    "Caps_Lock": 0x39,
+    "Num_Lock": 144,
+    "Mute": 0xef,
 
-    "up": 0x52,
-    "down": 0x51,
-    "left": 0x50,
-    "right": 0x4F,
+    "Home": 74,
+    "End": 77,
+    "PgUp": 75,
+    "PgDown": 78,
 
-    "0": 0x27,
+    "Up": 0x52,
+    "Down": 0x51,
+    "Left": 0x50,
+    "Right": 0x4F,
+
+    "`": 0x35,
     "1": 0x1E,
+    "!": [0x1E],
     "2": 0x1F,
+    "@": [0x1F],
     "3": 0x20,
+    "#": [0x20],
     "4": 0x21,
+    "$": [0x21],
     "5": 0x22,
+    "%": [0x22],
     "6": 0x23,
+    "^": [0x23],
     "7": 0x24,
+    "&": [0x24],
     "8": 0x25,
     "9": 0x26,
+    "(": [0x26],
+    "0": 0x27,
+    ")": [0x27],
+
     "=": 0x2E,
     "+": 0x57,
     "-": 0x56,
     "*": 0x55,
     "/": 0x54,
-    "%": 0x22,
-    "&": 0x24,
-    "|": 0x31,
-    "!": 0x1E,
-    "^": 0x23,
+    "|": [0x31],
 
     "'": 0x34,
-    "<": [0x36],
-    ">": [0x37],
-    "#": 0x20,
-    "@": 0x1F,
-    "?": 0x38,
-    "(": [0x26],
-    ")": [0x27],
-    ".": 0x37,
+    '"': [0x34],
+    "?": [0x38],
     ",": 0x36,
+    "<": [0x36],
+    ".": 0x37,
+    ">": [0x37],
     "\\": 0x64,
     "[": 0x2F,
-    "]": 0x30,
-    ":": [0x33],
-    ";": 0x33,
-    "$": 0x21,
     "{": [0x2F],
+    "]": 0x30,
     "}": [0x30],
-    '"': [0x34],
+    ";": 0x33,
+    ":": [0x33],
 
-    "f1": 0x3A,
-    "f2": 0x3B,
-    "f3": 0x3C,
-    "f4": 0x3D,
-    "f5": 0x3E,
-    "f6": 0x3F,
-    "f7": 0x40,
-    "f8": 0x41,
-    "f9": 0x42,
-    "f10": 0x43,
-    "f11": 0x44,
-    "f12": 0x45,
+    "F1": 0x3A,
+    "F2": 0x3B,
+    "F3": 0x3C,
+    "F4": 0x3D,
+    "F5": 0x3E,
+    "F6": 0x3F,
+    "F7": 0x40,
+    "F8": 0x41,
+    "F9": 0x42,
+    "F10": 0x43,
+    "F11": 0x44,
+    "F12": 0x45,
 }
